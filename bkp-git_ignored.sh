70e5c37a7576c2b553a624738538701ae812fb0f
Elias Silva <pro.eliasdns@gmail.com>
2022-05-11 20:01:14 -0300

    fix: bkp-git_ignored.sh: mkdir .bkp

--- bkp-git_ignored.sh
+++ bkp-git_ignored.sh
@@ -5,6 +5,7 @@ exclude="\.bkp/.*"
 dir=$(dirname "$0")/.bkp
 
 pushd "$dir/.." 1>/dev/null 2>&1
+mkdir -p .bkp
 
 git ls-files --others --ignored --exclude-standard | egrep -v "$exclude" | tar -czvf "$dir/gitignored-$(date '+%Y_%m_%d_%H%M').tar.gz" --verbatim-files-from --files-from=-
 
