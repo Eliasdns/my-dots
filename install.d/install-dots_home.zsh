252f96954ee9ac3a1b59e6dc733c59a8defbe1d7
Elias Silva <pro.eliasdns@gmail.com>
2022-05-12 01:52:56 -0300

    adding kitty dotfiles; etc

--- install.d/install-dots_home.zsh
+++ install.d/install-dots_home.zsh
@@ -10,13 +10,22 @@
 source "$(dirname $0)/_update_config_file.zsh"
 
 # --- powerlevel10k:
-cp -i "$(dirname $0)/home/.p10k.zsh" ~
+cp -i "$(dirname $0)/home/p10k/.p10k.zsh" ~
+sudo chown $USER:$USER ~/.p10k.zsh
+sudo chmod 644 ~/.p10k.zsh
 
 config_file="$HOME/.zshrc"
 
-partial_file="$(dirname $0)/home/_p10k_zshrc_tail.zsh"
+partial_file="$(dirname $0)/home/p10k/_p10k_zshrc_tail.zsh"
 update_config_file "$config_file" "$partial_file"
 
-partial_file="$(dirname $0)/home/_p10k_zshrc_head.zsh"
+partial_file="$(dirname $0)/home/p10k/_p10k_zshrc_head.zsh"
 update_config_file_head "$config_file" "$partial_file"
+
+# --- kitty:
+mkdir -p ~/.config/kitty
+cp -i ./home/kitty/* ~/.config/kitty
+sudo chown $USER:$USER ~/.config/kitty/*
+sudo chmod 644 ~/.config/kitty/kitty.conf
+sudo chmod 600 ~/.config/kitty/current-theme.conf
 
