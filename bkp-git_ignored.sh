9a77580040683eddebde5498b33bd7a02717de72
Elias Silva <pro.eliasdns@gmail.com>
2023-09-23 03:41:28 -0300

    bkp-git_ignored.sh: fixed not backuping untracked files

--- bkp-git_ignored.sh
+++ bkp-git_ignored.sh
@@ -7,7 +7,7 @@ dir=$(dirname "$0")/.bkp
 pushd "$dir/.." 1>/dev/null 2>&1
 mkdir -p .bkp
 
-git ls-files --others --ignored --exclude-standard | egrep -v "$exclude" | tar -czvf "$dir/gitignored-$(date '+%Y_%m_%d_%H%M').tar.gz" --verbatim-files-from --files-from=-
+git ls-files --others | egrep -v "$exclude" | tar -czvf "$dir/gitignored-$(date '+%Y_%m_%d_%H%M').tar.gz" --verbatim-files-from --files-from=-
 
 popd 1>/dev/null 2>&1
 unset exclude
