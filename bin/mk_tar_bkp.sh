1303fa63b122e45561f50eeeb65e55ab3b1c77de
Elias Silva <pro.eliasdns@gmail.com>
2026-03-02 16:21:06 -0300

    bin/mk_tar_bkp.sh: update the script

--- bin/mk_tar_bkp.sh
+++ bin/mk_tar_bkp.sh
@@ -1,27 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 make_tar_bkp() {
     if [ "$1" = "--help" -o "$1" = "-h" ]; then
-        echo "Usage: $0 <destination dir> <origin path>"
+        echo "Usage: $0 <source path 1> [<source path 2>...] <destination dir>"
         return 0
     elif [ -z $1 ]; then
-      echo Pass the backup destination dir as first argument!
+      echo Pass one or more backup source paths as the first arguments!
       exit 1
     fi
     if [ -z $2 ]; then
-      echo Pass the backup origin path as second argument!
+      echo Pass the backup destination dir as the last argument!
       exit 1
     fi
 
-    orig_path=$2
-    _orig_path_real=$(realpath "$orig_path")
+    src_paths=()
+    for i in "${@:1:$#-1}"; do
+        src_paths+=("${i%/}")
+    done
+    _src_paths_filenames=()
+    for i in "${src_paths[@]}"; do
+        _src_paths_filenames+="${i##*/}-"
+    done
 
-    # dest_dir=$(dirname "$0")/bkp
-    dest_dir=$1
-    dest_filename="${_orig_path_real##*/}-$(date '+%Y_%m_%d_%H%M').tar.gz"
+    dst_dir=${@: -1}
+    dst_filename="${_src_paths_filenames[@]}-$(date '+%Y_%m_%d_%H%M').tar.gz"
 
-    mkdir -p "$dest_dir"
-    tar -czvf "$dest_dir/$dest_filename" "$orig_path"
+    echo "[*] Running: mkdir -p" "$dst_dir"
+    mkdir -p "$dst_dir"
+    echo "[*] Running: tar -czvf" "$dst_dir/$dst_filename" "${src_paths[@]}"
+    tar -czvf "$dst_dir/$dst_filename" "${src_paths[@]}"
 }
 
 make_tar_bkp $@
+
